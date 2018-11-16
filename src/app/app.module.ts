68c4a84f7b50ba28f933ff8e5feb02e9ccb96e3d
Land <landjo@corp.saic.com>
2018-11-16 13:18:51 -0600

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,36 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatMenuModule, MatButtonModule, MatIconModule, MatCardModule, MatSidenavModule, MatToolbarModule } from '@angular/material';
+
+import { AppComponent } from './app.component';
+
+import 'hammerjs';
+import { MenudemoComponent } from './menudemo/menudemo.component';
+import { SidenavdemoComponent } from './sidenavdemo/sidenavdemo.component';
+import { ToolbardemoComponent } from './toolbardemo/toolbardemo.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MenudemoComponent,
+    SidenavdemoComponent,
+    ToolbardemoComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatMenuModule,
+    MatButtonModule,
+    MatIconModule,
+    MatCardModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatMenuModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
